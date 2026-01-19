391c2c9119efe0832e0920209ee60232dee0cdad
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-01-19 19:28:45 +0200

    router beta commit

---
+++ src/routes/auth.router.ts
@@ -0,0 +1,13 @@
+import { Express } from "express";
+import { Router } from "express";
+
+
+const router = Router();
+
+const authService = new AuthService()
+const authController = new AuthController(authService)
+
+router.post('/register',authController.register)
+router.post('login',authService.login)
+
+export default router
