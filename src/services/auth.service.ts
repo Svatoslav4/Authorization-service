59b4b786ec0e6e6d0249a0b7b95dcf4ab278df34
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-01-21 19:35:53 +0200

    Beta auth.service.ts commit

---
+++ src/services/auth.service.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+interface User {
+    id: number,
+    email: string,
+    password: string|number
+}
+
+
+export class AuthService {
+    private readonly users: User[] = []
+
+    
+
+
+}
