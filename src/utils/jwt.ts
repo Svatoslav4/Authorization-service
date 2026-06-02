7d7a699cb7c8d74b7a872b416721f7768596cccc
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-06-02 15:14:54 +0300

    utils commit

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import  jwt  from "jsonwebtoken";
+
+export const generateAccessToken = (userId: string) => {
+    return jwt.sign(
+        {userId},
+        process.env.JWT_ACCESS_SECRET!,
+        {
+            expiresIn: "15w"
+        }
+    )
+}
+
+export const generateRefreshToken = (userId: string) => {
+    return jwt.sign(
+        {userId},
+        process.env.JWT_REFRESH_SECRET!,
+        {
+            expiresIn: "30d"
+        }
+    )
+}
