e4df4dbc525768b8daabc52750e75eb8d940b1db
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-06-02 16:14:22 +0300

    auth validation

---
+++ src/models/auth/auth.validation.ts
@@ -0,0 +1,13 @@
+import { email, z } from "zod"
+
+export const registerSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().min(2)
+})
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
