b1693fbf1f910e7834e5494e8bf6d26198aa6f31
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-03 11:22:23 +0300

    rate-limit add in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-import express from "express"
-import cors from "cors"
-import userRoutes from "./models/user/user.routes"
-import authRoutes from "./models/auth/auth.routes"
+import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
+import authRoutes from "./models/auth/auth.routes";
+import userRoutes from "./models/user/user.routes";
+const app = express();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
+app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
+app.use("/logout",authRoutes)
 
-
-export default app
+export default app;
