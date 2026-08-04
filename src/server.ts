ac93b2dd72c0b298be977404aae23161b62a3af4
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-04 15:31:34 +0300

    colors add

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 import app from './app'
+import colors from 'colors'
 
 dotenv.config()
 
 const port = process.env.port || 5000
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(colors.green(`Server running on http://localhost:${port}`));
 }) 
