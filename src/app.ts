7136171aace1d5a3e082104f9f111b5d0b7b2366
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-04 15:31:15 +0300

    helmet add

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
 import authRoutes from "./models/auth/auth.routes";
 import userRoutes from "./models/user/user.routes";
-import helmet from "helmet";
 
 const app = express();
 
-app.use(helmet())
+
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
-app.use("/auth",authRoutes);
-app.use("/users",userRoutes);
-app.use("/logout",authRoutes)
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use('/change-password',authRoutes)
 
 export default app;
