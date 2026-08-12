5c989566d8da2cbc6cb3ad636d249ec0ddd2b2e8
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-12 16:05:33 +0300

    settings file update

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+    preset: "ts-jest",
+    testEnvironment: "node",
+
+    roots: ["<rootDir>/src/tests"],
+
+    testMatch: [
+        "**/*.test.ts"
+    ],
+
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "/dist/"
+    ],
+
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1"
+    }
 };
