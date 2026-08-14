6f6a639ab86cfe2af679ab9bfa9e50d5392bf399
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-14 17:28:12 +0300

    cod update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import dotenv from 'dotenv'
 import app from './app'
 import colors from 'colors'
+import { connectRedis } from '././redis/redis';
 
 dotenv.config()
 
 const port = process.env.port || 5000
 
-app.listen(port, () => {
-    console.log(colors.green(`Server running on http://localhost:${port}`));
-}) 
+const startServer = async() => {
+    try {
+        await connectRedis()
+        app.listen(port, () => {
+            console.log(colors.green(`Server running on http://localhost:${port}`));
+        }) 
+    }
+    catch(error) {
+        console.error('Failed running server',error)
+        process.exit(1)
+    }
+}
+
+startServer()
