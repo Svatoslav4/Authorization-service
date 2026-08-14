7f96c613503c9eb63f4d0ab5ed8d8155de87e0bc
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-14 15:37:40 +0300

    google.ts update

--- src/utils/google.ts
+++ src/utils/google.ts
@@ -2,7 +2,9 @@ import { OAuth2Client } from "google-auth-library";
 
 export const GoogleTokenVerify = async (token: string) => {
   try {
-    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    const client = new OAuth2Client(
+      process.env.GOOGLE_CLIENT_ID
+    );
 
     const ticket = await client.verifyIdToken({
       idToken: token,
@@ -11,7 +13,11 @@ export const GoogleTokenVerify = async (token: string) => {
 
     return ticket.getPayload();
   } catch (error) {
-    console.error("Google Token Verification Failed:", error);
+    console.error(
+      "Google Token Verification Failed:",
+      error
+    );
+
     return null;
   }
 };
