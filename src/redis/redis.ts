e0f278022df0fda8f786995212f3015272277402
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-28 17:21:07 +0300

    redis update

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis'
 
 export const redis = createClient({
-    url: process.env.REDIS_URL || 'redis//localhost:6379'
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
 })
 
 redis.on('error',(error) =>{
@@ -10,7 +10,7 @@ redis.on('error',(error) =>{
 
 export const connectRedis = async() => {
     if(!redis.isOpen){
-        await redis.connect
+        await redis.connect()
     }
     console.log('Redis connected')
 }
