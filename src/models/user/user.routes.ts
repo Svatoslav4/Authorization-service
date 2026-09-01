3dad258c015700e9ab519c31cf875d0d7bf51e04
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-09-01 13:15:13 +0300

    user.routes.ts update

--- src/models/user/user.routes.ts
+++ src/models/user/user.routes.ts
@@ -5,6 +5,40 @@ import { authMiddleware } from "../../middlewares/auth.middleware"
 const router = Router()
 
 const controller = new UserController()
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management endpoints
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - Users
+ *     summary: Get all users
+ *     description: Returns a list of all registered users.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 router.get("/",authMiddleware,controller.getUsers)
 
 export default router
